f673a43c602a61faca96d0eb92a5ad5e1fdb3097
Mthabisi Ndlovu <m.ndlovu@alustudent.com>
2021-02-09 09:45:02 +0200

    Migrated to Node.js for my backend.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/.'))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`myfolio listening on port ${port}!`));
